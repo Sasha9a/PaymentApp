4384dc53ff24c7abbd6e8eae88179192c019918d
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-08-27 15:34:33 +0300

    Реализовал таблицу с возможностью сортировки и видимости

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
 
 // Components
 import { AppComponent } from './app.component';
@@ -12,6 +13,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
+import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from "primeng/multiselect";
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { MenuModule } from 'primeng/menu';
     BrowserAnimationsModule,
     ToolbarModule,
     MenuModule,
-    ButtonModule
+    ButtonModule,
+    TableModule,
+    FormsModule,
+    MultiSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Payment } from "../../models/Payment/payment.model";
 
 @Component({
   selector: 'app-table',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  payments: Payment[] = [];
+  cols: any[] = [];
+  selectedColumns: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.payments = [
+      new Payment(1, "Аренда хостинга", 250, 0),
+      new Payment(2, "Продление хостинга", 240, 10),
+      new Payment(3, "Покупка домена", 500, 299),
+      new Payment(4, "Продление хостинга", 200, 50),
+      new Payment(5, "Смена тарифного плана хостинга", 650, 49)
+    ];
+
+    this.cols = [
+      { field: 'id', header: 'Номер' },
+      { field: 'name', header: 'Название' },
+      { field: 'sum', header: 'Сумма' },
+      { field: 'discount', header: 'Скидка' }
+    ];
+
+    this.selectedColumns = this.cols;
   }
 
 }
